4c8bcd5215926faee9731dd0b810ad9db6c4f1b5
Pioncha28 <137203018+Pioncha28@users.noreply.github.com>
2023-06-21 00:23:21 +0200

    v4

--- src/main/java/org/example/test/CalculatorTest.java
+++ src/main/java/org/example/test/CalculatorTest.java
@@ -1,43 +1,55 @@
 package org.example.test;
 
 import org.example.calculator.Calculator;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
     @Test
     public void testAddition() {
         Calculator calculator = new Calculator();
         int result = calculator.add(2, 3);
-        Assertions.assertEquals(5, result);
+        if (result != 5) {
+            throw new AssertionError("Addition test failed. Expected: 5, but got: " + result);
+        }
     }
 
     @Test
     public void testSubtraction() {
         Calculator calculator = new Calculator();
         int result = calculator.subtract(5, 3);
-        Assertions.assertEquals(2, result);
+        if (result != 2) {
+            throw new AssertionError("Subtraction test failed. Expected: 2, but got: " + result);
+        }
     }
 
     @Test
     public void testMultiplication() {
         Calculator calculator = new Calculator();
         int result = calculator.multiply(4, 3);
-        Assertions.assertEquals(12, result);
+        if (result != 12) {
+            throw new AssertionError("Multiplication test failed. Expected: 12, but got: " + result);
+        }
     }
 
     @Test
     public void testDivision() {
         Calculator calculator = new Calculator();
         int result = calculator.divide(10, 2);
-        Assertions.assertEquals(5, result);
+        if (result != 5) {
+            throw new AssertionError("Division test failed. Expected: 5, but got: " + result);
+        }
     }
 
     @Test
     public void testDivisionByZero() {
         Calculator calculator = new Calculator();
-        Assertions.assertThrows(ArithmeticException.class, () -> {
+        try {
             calculator.divide(10, 0);
-        });
+            throw new AssertionError("Division by zero test failed. Expected ArithmeticException, but no exception was thrown.");
+        } catch (ArithmeticException e) {
+            // Test passed
+        }
     }
 }
