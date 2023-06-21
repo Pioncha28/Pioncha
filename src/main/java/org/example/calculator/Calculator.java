1e28acef884482f811b2738244f54f3ac7a855b9
Pioncha28 <137203018+Pioncha28@users.noreply.github.com>
2023-06-21 14:16:06 +0200

    v5

--- src/main/java/org/example/calculator/Calculator.java
+++ src/main/java/org/example/calculator/Calculator.java
@@ -1,18 +1,31 @@
 package org.example.calculator;
+/**
+ * Klasa reprezentująca prosty kalkulator.
+ */
 
 public class Calculator {
-    public int add(int a, int b) {
+/**
+ * Dodaje dwie liczby.
+ */
+ public int add(int a, int b) {
         return a + b;
     }
-
+/**
+ * Odejmuje jedną liczbę od drugiej.
+ */
     public int subtract(int a, int b) {
         return a - b;
     }
+/**
+ * Mnoży dwie liczby.
+ */
 
-    public int multiply(int a, int b) {
+ public int multiply(int a, int b) {
         return a * b;
     }
-
+/**
+ * Dzieli jedną liczbę przez drugą.
+ */
     public int divide(int a, int b) {
         if (b == 0) {
             throw new ArithmeticException("Cannot divide by zero");
